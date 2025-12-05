00e7fa18e5388a69db08f04150827e75458636f7
TonmoyZohani <tonmoyzohani@gmail.com>
2025-12-05 18:27:56 +0600

    controllers and routes created

--- src/modules/todo/todo.routes.ts
+++ src/modules/todo/todo.routes.ts
@@ -1,2 +1,12 @@
-import {Router} from 'express';
-import { todoController } from './todo.controller'
+import { Router } from "express";
+import { todoController } from "./todo.controller";
+
+const todoRouter = Router();
+
+todoRouter.post("/", todoController.createTodo);
+todoRouter.get("/", todoController.getTodos);
+todoRouter.get("/:id", todoController.getSingleTodo);
+todoRouter.put("/:id", todoController.updateTodo);
+todoRouter.delete("/:id", todoController.deleteTodo);
+
+export const todoRoutes = todoRouter;
