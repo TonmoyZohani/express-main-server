ca669eb4422c685da414d7f55a44687934e4b5e6
TonmoyZohani <tonmoyzohani@gmail.com>
2025-12-06 13:53:53 +0600

    auth controller done

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,1 +1,21 @@
-import 
+import { Request, Response } from "express";
+import { authServices } from "./auth.service";
+
+const loginUser = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  try {
+    const result = await authServices.loginUser({ email, password });
+    res.status(200).json({
+      success: true,
+      message: "Login Successful",
+      data: result,
+    });
+  } catch (err: any) {
+    res.status(500).json({ success: false, mesage: err.message });
+  }
+};
+
+export const authControllers = {
+  loginUser,
+};
